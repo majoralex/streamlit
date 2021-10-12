73020f85aa1b8d2f8ed289e1fe25fc4e17033080
Alex <alex.anthony.major@gmail.com>
2021-10-12 01:38:28 -0400

    did a lot

--- string_comparison_app/main.py
+++ string_comparison_app/main.py
@@ -30,15 +30,44 @@ def main():
     st.set_page_config(layout="wide")
     radio_options = ['1 Excel File', '2 Excel File']
     radio_button_1 = st.sidebar.radio("Select an Option", radio_options)
+    edit_based_algorithms ={'Hamming': 'hamming',
+                            'MLIPNS': 'mlipns',
+                            'Levenshtein': 'levenshtein',
+                            'Damerau-Levenshtein': 'damerau_levenshtein',
+                            'Jaro-Winkler': 'jaro_winkler',
+                            'Strcmp95': 'strcmp95',
+                            'Needleman-Wunsch': 'needleman_wunsch',
+                            'Gotoh': 'gotoh',
+                            'Smith-Waterman': 'smith_waterman'
+                            }
+    # token_based_algorithms = ['Hamming', 'MLIPNS', 'Levenshtein', 'Damerau-Levenshtein','Jaro-Winkler', 'Strcmp95', 'Needleman-Wunsch', 'Gotoh']
+
+
+    slider_button_1 = st.sidebar.slider("Select a Similarity Threshold")
+    slider_button_2 = st.sidebar.slider("Select a Record Limit", max_value=20)
+    multiselect_sidebar_1 = st.sidebar.multiselect("Select one or more algorithms", edit_based_algorithms)
+    # multiselect_sidebar_2 = st.sidebar.multiselect("Select one or more 'Token based' algorithms", token_based_algorithms)
+    # multiselect_sidebar_3 = st.sidebar.multiselect("Select one or more 'Sequence based' algorithms", text_algorithms)
+    # multiselect_sidebar_4 = st.sidebar.multiselect("Select one or more 'Compression based' algorithms", text_algorithms)
+    # multiselect_sidebar_5 = st.sidebar.multiselect("Select one or more 'Phonetic' algorithms", text_algorithms)
+    multiselect_algorithm_methods = list(map(edit_based_algorithms.get, multiselect_sidebar_1))
+
+    if multiselect_sidebar_1 is not None:
+        st.write(multiselect_algorithm_methods)
+
     col1, col2 = st.columns((1, 1))
     if radio_button_1 == radio_options[0]:
         file = col1.file_uploader(label='Select a file')
         st.markdown("***")
-        if file is not None:
+        if file is not None and multiselect_sidebar_1 is not None:
             df = pd.read_excel(file, engine='openpyxl')
-            col1.multiselect("Multiselect", df.columns.values)
-            col1 = st.markdown("***")
-            col1.write(df)
+            col1, col2 = st.columns((1, 1))
+            option_1 = col1.selectbox("Select header from first file", convert_tuple(df.columns.values))
+            option_2 = col2.selectbox("Select item from second file", convert_tuple(df.columns.values))
+
+            df = find_similarities(df_1=df, df_2=df, match_on=[option_1, option_2], set_similarity_threshold=slider_button_1/100, set_record_limit=3, algo_list=multiselect_algorithm_methods)
+            st.write(f"With a Similarity Threshold of {slider_button_1} and a Maximum of {slider_button_2} matches per record in the first file")
+            st.write(df)
         else:
             col1.write("")
     elif radio_button_1 == radio_options[1]:
@@ -46,18 +75,22 @@ def main():
         file_1 = col1.file_uploader(label='Select a file')
         file_2 = col2.file_uploader(label='Select another file')
 
-        if file_1 is not None and file_2 is not None:
+        if file_1 is not None and file_2 is not None and multiselect_sidebar_1 is not None:
 
             first_df = pd.read_excel(file_1, engine='openpyxl')
             second_df = pd.read_excel(file_2, engine='openpyxl')
             # option_1 = col1.multiselect("Multiselect", [*df_1.columns.values, *df_2.columns.values]
             option_1 = col1.selectbox("Select header from first file", convert_tuple(first_df.columns.values))
-            option_2 = col2.selectbox("Select header from second file", convert_tuple(second_df.columns.values))
+            option_2 = col2.selectbox("Select item from second file", convert_tuple(second_df.columns.values))
             col1, col2 = st.columns((1, 1))
-            df = find_similarities(df_1=first_df, df_2=second_df, match_on=[option_1, option_2], set_similarity_threshold=0.7, set_record_limit=3)
-            st.write(df)
-            # col1.write(df_1)
-            # col2.write(df_2)
+            df = find_similarities(df_1=first_df, df_2=second_df, match_on=[option_1, option_2], set_similarity_threshold=slider_button_1/100, set_record_limit=3, algo_list=multiselect_algorithm_methods)
+            col1, col2 = st.columns((5, 2))
+            col1.write(f"With a Similarity Threshold of {slider_button_1} and a Maximum of {slider_button_2} matches per record in the first file")
+
+
+
+            st.write(df.head(1000))
+
         else:
             st.markdown("***")
 
@@ -66,5 +99,6 @@ def main():
 
 
 if __name__ == "__main__":
+
     main()
 
--- string_comparison_app/streamlit_scripts.py
+++ string_comparison_app/streamlit_scripts.py
@@ -15,12 +15,11 @@ import textdistance
 #         self.
 #         return "this is a test"
 
-def test():
-    return "This is a test!"
 
 def find_similarities(df_1: pd.DataFrame, df_2: pd.DataFrame, match_on: list,
     set_record_limit: int,
-    set_similarity_threshold: int): # ToDo: keep in case we need
+    set_similarity_threshold: int, # ToDo: keep in case we need
+    algo_list: list):
     """
     The purpose of this script is to find the similarity between 2 strings from the textdistance library, using Levenshtein
     distancing and other algorithms.
@@ -34,17 +33,29 @@ def find_similarities(df_1: pd.DataFrame, df_2: pd.DataFrame, match_on: list,
     for df_2_index, df_2_row in df_2.iterrows():
         for df_1_index, df_1_row in df_1.iterrows():
             Levenshtein_sim = textdistance.levenshtein.normalized_similarity(df_2_row[match_on[1]], df_1_row[match_on[0]])
+
             similarity_df.append({
                 'df_1_index': df_1_index,
                 'df_2_index': df_2_index,
                 'df_1_row': df_1_row[match_on[0]],
                 'df_2_row': df_2_row[match_on[1]],
-                'text_similarity': Levenshtein_sim
+                'text_similarity': Levenshtein_sim,
+                'test': algo_list[0]
             })
+            # for index, item in enumerate(algo_list):
+            #     similarity_df[f"{algo_list[index]}"].append(algo_list[item])
+            # similarity_df[f"{algo_list[item]}"] = algo_list[index]
 
     final_df = pd.DataFrame(similarity_df).sort_values(by=['df_1_index', 'text_similarity']).drop_duplicates()
-
     final_df = final_df[final_df['text_similarity'].astype(float) >= set_similarity_threshold]
+
+    # last_df = pd.DataFrame()
+    # unique_df_1_index = final_df['df_1_index'].unique()
+    # print(unique_df_1_index)
+
+    # for index, row in enumerate(unique_df_1_index): # Sort by Algorithm
+    #     df_loop = final_df[final_df[row]].head(set_record_limit)
+    #     last_df.append(df_loop)
 
 
     return final_df
