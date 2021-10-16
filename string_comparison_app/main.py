97fb9c6477806e848003e1a48ff1755be0d63341
Alex <alex.anthony.major@gmail.com>
2021-10-16 15:24:57 -0400

    did a lot

--- string_comparison_app/main.py
+++ string_comparison_app/main.py
@@ -3,6 +3,7 @@ import pandas as pd
 import textdistance
 from streamlit_scripts import find_similarities, textdistance_streamlit_code
 
+import time
 
 # df = pd.DataFrame()
 # data = {
@@ -15,13 +16,6 @@ from streamlit_scripts import find_similarities, textdistance_streamlit_code
 #
 # st.write("Hello World")
 # st.write(test())
-
-def v_spacer(height, sb=False) -> None:
-    for _ in range(height):
-        if sb:
-            st.sidebar.write('\n')
-        else:
-            st.write('\n')
 
 def convert_tuple(list):
     return (*list, )
@@ -45,19 +39,39 @@ def main():
 
 
     slider_button_1 = st.sidebar.slider("Select a Similarity Threshold")
-    slider_button_2 = st.sidebar.slider("Select a Record Limit", max_value=20)
+    slider_button_2 = st.sidebar.slider("Select a Record Limit", max_value=100)
     multiselect_sidebar_1 = st.sidebar.multiselect("Select one or more algorithms", edit_based_algorithms)
     # multiselect_sidebar_2 = st.sidebar.multiselect("Select one or more 'Token based' algorithms", token_based_algorithms)
     # multiselect_sidebar_3 = st.sidebar.multiselect("Select one or more 'Sequence based' algorithms", text_algorithms)
     # multiselect_sidebar_4 = st.sidebar.multiselect("Select one or more 'Compression based' algorithms", text_algorithms)
     # multiselect_sidebar_5 = st.sidebar.multiselect("Select one or more 'Phonetic' algorithms", text_algorithms)
     multiselect_algorithm_methods = list(map(edit_based_algorithms.get, multiselect_sidebar_1))
-    st.header("textdistance app")
-    with st.expander("See explanation and instructions about the project"):
-        st.subheader("README")
-        st.write("The purpose of this script is to...")
+    new_title = '<p style="font-family:sans-serif; color:Red; font-size: 60px;">textdistance app</p>'
+    col1, col2, col3 = st.columns((2, 1, 1))
+
+    col1.markdown(new_title, unsafe_allow_html=True)
+    with col2.expander("About this App"):
+        st.subheader("Goal")
+        st.write("""The purpose of this app is to democratize the use of the [textdistance library](https://pypi.org/project/textdistance/). The script in the background
+        takes one or more tabular datasets **(-> DataFrames)**, processes the data based on user specifications in the sidebar and the fields chosen to measure. 
+        The *find_similarities()* function found at the bottom of this app to get the """)
+        st.subheader("Outcomes")
+        st.write("(A) Normalized Similarity of 2 Fields with one or more algorithms from the textdistance library.")
+        st.write("(B) A downloadable CSV of the Output")
+    with col3.expander("How to use this app"):
+        st.subheader("Files")
+        st.write("""(A) The files can be excel and/or csv.""")
+        st.write("""(B) If your data is not on the first row of the data, make sure to specify the starting row of the data in the expander below their respective file upload component""")
+        st.write("""(C) This app uses the first file as a *base*, to match against the selected field from the second file""")
+        st.subheader("Parameters")
+        st.write("(A) **Similarity Threshold**: this is the normalized similarity between")
+        st.write("(B) **Record Limit**: this is the normalized similarity between")
+        st.write("(C) **Algorithms**: [This is a list of availble algorithms and all methods](https://pypi.org/project/textdistance/)")
+
+
     col1, col2 = st.columns((1, 1))
     if radio_button_1 == radio_options[0]:
+
         file = col1.file_uploader(label='Select a file')
         st.markdown("***")
         if file is not None and multiselect_sidebar_1 is not None:
@@ -73,29 +87,49 @@ def main():
             col1.write("")
     elif radio_button_1 == radio_options[1]:
         col1, col2, col3, col4 = st.columns((2, 1, 2, 1))
+
         file_1 = col1.file_uploader(label='Select a file')
+
         file_2 = col3.file_uploader(label='Select another file')
 
-        if file_1 is not None and file_2 is not None and multiselect_sidebar_1 is not None:
+        col1, col2, col3, col4 = st.columns((3, 1, 3, 1))
+        with col1.expander("Customize file #1 upload"):
+            st.number_input("Enter in starting row ", 0)
+        with col3.expander("Customize file #2 upload"):
+            st.number_input("Enter in starting row  ", 0)
 
+        if file_1 is not None and file_2 is not None and multiselect_sidebar_1 is not None:
             first_df = pd.read_excel(file_1, engine='openpyxl')
             second_df = pd.read_excel(file_2, engine='openpyxl')
             # option_1 = col1.multiselect("Multiselect", [*df_1.columns.values, *df_2.columns.values]
-            col1, col2, col3, col4 = st.columns((1, 2, 1, 2))
+            col1, col2, col3, col4 = st.columns((1.5, 2, 1.5, 2))
             option_1 = col1.selectbox("Select header from first file", convert_tuple(first_df.columns.values))
             option_2 = col3.selectbox("Select item from second file", convert_tuple(second_df.columns.values))
-            if st.button('Refresh Data'):
-                df = find_similarities(df_1=first_df, df_2=second_df, match_on=[option_1, option_2], set_similarity_threshold=slider_button_1/100, set_record_limit=3, algo_list=multiselect_algorithm_methods)
-                col1, col2 = st.columns((5, 2))
-                col1.write(f"With a Similarity Threshold of {slider_button_1} and a Maximum of {slider_button_2} matches per record in the first file")
-                st.write(df)
-            col1, col2 = st.columns((1, 1))
-            code_checkbox = col1.checkbox("View Source Code")
-            if code_checkbox:
-                st.code(textdistance_streamlit_code(), language='python')
+            col1, col2, col3 = st.columns((1, 1, 5))
+            if col1.button('Refresh Data'):
+                if multiselect_sidebar_1:
+                    with st.spinner(text='Hold on, trying to match your data...'):
+                        df = find_similarities(df_1=first_df, df_2=second_df, match_on=[option_1, option_2], set_similarity_threshold=slider_button_1/100, set_record_limit=slider_button_2, algo_list=multiselect_algorithm_methods)
+                            # df = find_similarities(df_1=first_df, df_2=second_df, match_on=[option_1, option_2], set_similarity_threshold=slider_button_1/100, set_record_limit=3, algo_list=multiselect_algorithm_methods)
+                        # col1, col2 = st.columns((5, 2))
+
+                        col2.download_button(label="Download Data", data=df.to_csv().encode('utf-8'),
+                                             file_name='textdistance_streamlit_output.csv', mime='text/csv')
+                        output_result = f'<p style="font-family:sans-serif; color:Red; font-size: 16px;">Similarity Threshold of {slider_button_1} | Record limit of {slider_button_2}</p>'
+                        st.markdown(output_result, unsafe_allow_html=True)
+                        st.write(df)
+                        st.balloons()
+                else:
+                    st.error("Please select a textdistance algorithm from the sidebar!")
+
+
+
+
 
         else:
             st.markdown("***")
+            with st.expander("View Source Code"):
+                st.code(textdistance_streamlit_code(), language='python')
 
 
     # print(dir(st))
--- string_comparison_app/streamlit_scripts.py
+++ string_comparison_app/streamlit_scripts.py
@@ -5,7 +5,7 @@ import textdistance
 
 def find_similarities(df_1: pd.DataFrame, df_2: pd.DataFrame, match_on: list,
     set_record_limit: int,
-    set_similarity_threshold: int, # ToDo: keep in case we need
+    set_similarity_threshold: int,
     algo_list: list):
     """
     The purpose of this script is to find the similarity between 2 strings from the textdistance library, using Levenshtein
@@ -23,14 +23,14 @@ def find_similarities(df_1: pd.DataFrame, df_2: pd.DataFrame, match_on: list,
                 f"textdistance.{algo_list[0]}.normalized_similarity(df_2_row[match_on[1]], df_1_row[match_on[0]])")
 
             similarity_df.append({
-                'df_1_index': df_1_index,
-                'df_2_index': df_2_index,
+                f'{match_on[0]}_index': df_1_index,
+                f'{match_on[1]}_index': df_2_index,
                 match_on[0]: df_1_row[match_on[0]],
                 match_on[1]: df_2_row[match_on[1]],
                 f'{algo_list[0]}_score': first_algo,
             })
 
-    final_df = pd.DataFrame(similarity_df).sort_values(by=['df_1_index', f'{algo_list[0]}_score']).drop_duplicates()
+    final_df = pd.DataFrame(similarity_df).sort_values(by=[f'{match_on[0]}_index', f'{algo_list[0]}_score']).drop_duplicates()
 
     for index, row in final_df.iterrows():
         for algo_index, algo in enumerate(algo_list):
@@ -40,8 +40,9 @@ def find_similarities(df_1: pd.DataFrame, df_2: pd.DataFrame, match_on: list,
             final_df.at[index, f'{algo}_score'] = algo_output
 
     final_df = final_df[final_df[f'{algo_list[0]}_score'].astype(float) >= set_similarity_threshold]
-
-    return final_df
+    grouped_df = final_df.groupby(f'{match_on[0]}_index')
+    final_df_record_limit = grouped_df.head(set_record_limit)
+    return final_df_record_limit
 
 def textdistance_streamlit_code():
     return """
@@ -63,31 +64,31 @@ def textdistance_streamlit_code():
                     :returns full_df, threshold_df: full_df shows the the records with the top nth records based on record limit
                     threshold_df is filtered from full_df, but cuts off the dataframe depending on Levensthein ratio
                     \"""
-                    similarity_df = []
-                    for df_2_index, df_2_row in df_2.iterrows():
-                        for df_1_index, df_1_row in df_1.iterrows():
-                            first_algo = eval(
-                                f"textdistance.{algo_list[0]}.normalized_similarity(df_2_row[match_on[1]], df_1_row[match_on[0]])")
-                
-                            similarity_df.append({
-                                'df_1_index': df_1_index,
-                                'df_2_index': df_2_index,
-                                match_on[0]: df_1_row[match_on[0]],
-                                match_on[1]: df_2_row[match_on[1]],
-                                f'{algo_list[0]}_score': first_algo,
-                            })
-                
-                    final_df = pd.DataFrame(similarity_df).sort_values(by=['df_1_index', f'{algo_list[0]}_score']).drop_duplicates()
-                
-                    for index, row in final_df.iterrows():
-                        for algo_index, algo in enumerate(algo_list):
-                            algo_output = eval(
-                                f"textdistance.{algo_list[algo_index]}.normalized_similarity(final_df[match_on[0]][{index}], final_df[match_on[1]][{index}])")
-                            print(f'{algo_list[algo_index]}_score', algo, "Output -> ", algo_output)
-                            final_df.at[index, f'{algo}_score'] = algo_output
-                
-                    final_df = final_df[final_df[f'{algo_list[0]}_score'].astype(float) >= set_similarity_threshold]
-                
-                    return final_df
+                similarity_df = []
+                for df_2_index, df_2_row in df_2.iterrows():
+                    for df_1_index, df_1_row in df_1.iterrows():
+                        first_algo = eval(
+                            f"textdistance.{algo_list[0]}.normalized_similarity(df_2_row[match_on[1]], df_1_row[match_on[0]])")
+            
+                        similarity_df.append({
+                            f'{match_on[0]}_index': df_1_index,
+                            f'{match_on[1]}_index': df_2_index,
+                            match_on[0]: df_1_row[match_on[0]],
+                            match_on[1]: df_2_row[match_on[1]],
+                            f'{algo_list[0]}_score': first_algo,
+                        })
+            
+                final_df = pd.DataFrame(similarity_df).sort_values(by=['df_1_index', f'{algo_list[0]}_score']).drop_duplicates()
+            
+                for index, row in final_df.iterrows():
+                    for algo_index, algo in enumerate(algo_list):
+                        algo_output = eval(
+                            f"textdistance.{algo_list[algo_index]}.normalized_similarity(final_df[match_on[0]][{index}], final_df[match_on[1]][{index}])")
+                        print(f'{algo_list[algo_index]}_score', algo, "Output -> ", algo_output)
+                        final_df.at[index, f'{algo}_score'] = algo_output
+            
+                final_df = final_df[final_df[f'{algo_list[0]}_score'].astype(float) >= set_similarity_threshold]
+            
+                return final_df
                 """
 
